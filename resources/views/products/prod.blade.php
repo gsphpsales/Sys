e5654b2b8b65eedf160a6c9ce4113b1b81392f8d
Gilson Sales <gilson.sales21@hotmail.com>
2019-06-18 19:07:17 -0300

    Create prod.blade.php
    model test

---
+++ resources/views/products/prod.blade.php
@@ -0,0 +1,165 @@
+@extends('layouts.app')
+
+@section('content')
+
+ <div class="main-panel">
+      @extends('layouts.navbar_admin')
+      
+
+          <!--teste table-->
+<div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                   <div class="card-icon">
+                    <i class="material-icons">library_books</i>
+                  </div>
+                  <h3 class="card-title ">Lista de Produtos</h3> 
+
+                  
+                </div>
+                  <p class="nav-link"><a href="prod_new"><i class="material-icons">note_add</i></a></p>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class=" text-primary">
+                        <th>
+                          ID
+                        </th>
+                        <th>
+                         Nome
+                        </th>
+                        <th>
+                          desc
+                        </th>
+                        <th>
+                          cat
+                        </th>
+                        <th>
+                         preço
+                        </th>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                            1
+                          </td>
+                          <td>
+                            Dakota Rice
+                          </td>
+                          <td>
+                            Niger
+                          </td>
+                          <td>
+                            Oud-Turnhout
+                          </td>
+                          <td class="text-primary">
+                            ATIVO
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            2
+                          </td>
+                          <td>
+                            Minerva Hooper
+                          </td>
+                          <td>
+                            Curaçao
+                          </td>
+                          <td>
+                            Sinaai-Waas
+                          </td>
+                          <td class="text-primary">
+                           ATIVO
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            3
+                          </td>
+                          <td>
+                            Sage Rodriguez
+                          </td>
+                          <td>
+                            Netherlands
+                          </td>
+                          <td>
+                            Baileux
+                          </td>
+                          <td class="text-primary">
+                           ATIVO
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            4
+                          </td>
+                          <td>
+                            Philip Chaney
+                          </td>
+                          <td>
+                            Korea, South
+                          </td>
+                          <td>
+                            Overland Park
+                          </td>
+                          <td class="text-primary">
+                            ATIVO
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            5
+                          </td>
+                          <td>
+                            Doris Greene
+                          </td>
+                          <td>
+                            Malawi
+                          </td>
+                          <td>
+                            Feldkirchen in Kärnten
+                          </td>
+                          <td class="text-primary">
+                        ATIVO
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            6
+                          </td>
+                          <td>
+                            Mason Porter
+                          </td>
+                          <td>
+                            Chile
+                          </td>
+                          <td>
+                            Gloucester
+                          </td>
+                          <td class="text-primary">
+                            ATIVO
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+      
+          close test-->
+          
+        </div>
+      </div>
+     
+@endsection
