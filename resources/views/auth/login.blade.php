92c31d16ef1e6018c9b6a849359504094639e4d9
Gilson Sales <gilson.sales21@hotmail.com>
2019-06-18 18:38:42 -0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app', ["current" => "login"])
 
 @section('content')
 <div class="container">
@@ -12,10 +12,8 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="E-mail" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -26,10 +24,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                      
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control" name="password" placeholder="Senha" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
