73031bb1768826d831d24159f6f4a7f8f646cf70
Gilson Sales <gilson.sales21@hotmail.com>
2019-06-25 11:14:41 -0300

    Update ProdController.php
    Store and show in  Controller

--- app/Http/Controllers/ProdController.php
+++ app/Http/Controllers/ProdController.php
@@ -29,26 +29,30 @@ class ProdController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
-        //
+       $prod = new Products();
+        $prod->name = $request->input('np');
+        $prod->ref = $request->input('ref');
+        $prod->price_c = $request->input('pc');
+        $prod->price_s = $request->input('pv');
+        $prod->img = $request->input('img');
+        $prod->cat_id = $request->input('catp');
+        $prod->status = "Ativo";
+        $prod->save();
+        return json_encode($prod);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function show($id)
     {
-        //
+        $prod =  Products::find($id);
+        if (isset($prod)) {
+            # code...
+            return json_encode($prod);
+        }
+        return response('Não encontrado', 404); 
     }
 
     /**
