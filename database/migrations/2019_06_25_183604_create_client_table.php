04dc0e8a39f73cd2bf1227ba1ab33afd70a3099f
Gilson Sales <gilson.sales21@hotmail.com>
2019-06-25 15:44:13 -0300

    Create 2019_06_25_183604_create_client_table.php

---
+++ database/migrations/2019_06_25_183604_create_client_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+          Schema::create('client', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('razao_soc');
+            $table->string('fantasia');
+            $table->string('cnpj');
+            $table->string('ie');
+            $table->string('email');
+            $table->string('celular');
+            $table->string('fixo');
+            $table->string('endereco');
+            $table->string('endereco_ent');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client');
+    }
+}
